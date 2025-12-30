e7fbe230ace5117104d14aa3edcf1fba399dd13d
Lucy-Lou <lucy.lou.agard@gmail.com>
2025-12-30 17:46:04 +0100

    nouveau script + fonctionnel

--- programmes/traitement_url.sh
+++ programmes/traitement_url.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/bash
 
 #Pour vérifier que l'utilisateur a bien entré un argument
-if [ $# -ne 2 ]
+if [ $# -ne 1 ]
 	then
-	echo "Ce programme demande deux arguments."
+	echo "Ce programme demande un argument."
 	exit
 fi
 
 #initialisation d'une variable comptant les lignes
 compteur=1
 
-SORTIE=$2
+LANGUE=$1
+
 #on crée le fichier dans lequel va être stocké notre résultat
->$2
+>../tableaux/${LANGUE}_site.html
 
 #on crée une variable qui va stocker nos données extraites dans un tableau pour les traiter ensuite
 TSV+=$'Ligne\tCode_HTTP\tEncodage\tMots\tURL\n'
@@ -22,33 +23,34 @@ while read -r line;
 do
 	#pour un des lien on a un soucis de redirection, donc on ajoute -L, avec -I on ne récupère que l'entête du site
 	# -s pour ne pas avoir la barre de progression, -o /dev/null pour récupérer le contenu de la page dans un fichier qui n'existe pas, donc on ne garde que le -w
-	echo "Traitement url $compteur"
 	code=$(curl -s -I -L -o /dev/null -w "%{http_code}" "$line")
 
 	#utiliser plusieur curl faisait bloquer wikimedia car trop de requêtes, j'en utilise donc seulement 1 que je met dans une variable contenu
-	echo "Traitement url $compteur."
 	contenu=$(curl -s -L "$line")
 
-	echo "Traitement url $compteur.."
-	encodage=$(echo "$contenu"| grep -i 'charset=' | sed 's/.*charset=//I')
+	encodage=$(echo "$contenu" | grep -ioP "charset=[\"\']?\K[\w-]+" | head -n 1)
+	if [ -z "$encodage" ]
+		then
+		encodage=$(curl -s -I -L "$line" | grep -i 'content-type' | sed 's/.*charset=//I' | tr -d '\r')
+	fi
+
 	#dans le cas la variable encodage est vide, au lieu de l'afficher tel quel on la remplie par "absence d'encodage"
 	if [ -z "$encodage" ]
 		then
 		encodage="Absence d'encodage"
 	fi
 
-	echo "Traitement url $compteur..."
 	mots=$(echo "$contenu"| wc -w)
 
 	#on ajoute nos infos dans la varibale TSV
-	echo "Traitement url $compteur...."
 	TSV+="${compteur}"$'\t'"${code}"$'\t'"${encodage}"$'\t'"${mots}"$'\t'"${line}"$'\n'
+	echo "Url $compteur traité"
 
 	#incrémentation compteur
 	compteur=$((compteur + 1))
 	#pour éviter de recevoir le code 429 "too many request" j'impose un temps de latence d'une seconde entre les requêtes
 	sleep 1
-done < "$1";
+done < "../URLs/${LANGUE}_url.txt";
 
 echo "Création du fichier html..."
 {
@@ -94,6 +96,6 @@ echo "      </table>"
 echo " 		</div>"
 echo "  </body>"
 echo "</html>"
-} >"$SORTIE"
+} >"../tableaux/${LANGUE}_site.html"
 
-echo "Fichier crée à : $2"
+echo "Fichier crée à : ../tableaux/${LANGUE}_site.html"
